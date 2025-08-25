c32d9b55220e70ec6c9c2bddb490125794c8e0e1
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2025-08-25 11:13:52 -0300

    first commit

---
+++ src/api/filme/controllers/filme.ts
@@ -0,0 +1,7 @@
+/**
+ * filme controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::filme.filme');
---
+++ src/api/filme/routes/filme.ts
@@ -0,0 +1,7 @@
+/**
+ * filme router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::filme.filme');
